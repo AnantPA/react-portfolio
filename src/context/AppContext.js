0d061bcdbbcf0436ce545dc5c72e0ba8259d5a47
AnantPA <anantpatel8901@gmail.com>
2024-12-20 20:06:04 +0530

    2

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -9,7 +9,6 @@ export const AppProvider = ({ children }) => {
     const handleTriggerAction = () => {
         setTriggerAction(prev => !prev);
     };
-    // New 
 
     useEffect(() => {
         console.log('111')
